c982787d71c1c5b4f9a12a26b359ec9715948643
tanukile <backpacker320@gmail.com>
2023-10-02 15:37:20 +0800

    first commit

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import "bootstrap/dist/css/bootstrap.min.css" // 載入 BS.css
+import "bootstrap" // 載入 BS.js
+
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
