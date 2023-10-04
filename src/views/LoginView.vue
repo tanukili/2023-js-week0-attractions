3216cd6aec6c229ecc30f757c8573df5aa2d1dc2
tanukile <backpacker320@gmail.com>
2023-10-04 11:43:49 +0800

    feat: 登入註冊

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -5,6 +5,30 @@ export default {
   components: {
     FrontHeader,
   },
+  data() {
+    return {
+      user: {
+        email: '',
+        password: '',
+      },
+    };
+  },
+  methods: {
+    login() {
+      console.log('登入');
+      this.axios
+        .post('http://localhost:3000/login', this.user)
+        .then(() => {
+          alert('登入成功');
+          this.user.email = '';
+          this.user.password = '';
+          window.location.href = '/';
+        })
+        .catch((err) => {
+          alert(`登入失敗：${err.response.data}`);
+        });
+    },
+  },
 };
 </script>
 
@@ -12,7 +36,25 @@ export default {
   <header>
     <FrontHeader />
   </header>
-  <h1>登入</h1>
+  <div class="container mt-5">
+    <h1>登入</h1>
+    <form action="#">
+      <div class="mb-3">
+        <label for="email" class="me-2">信箱</label>
+        <input type="email" id="email" v-model="user.email" />
+      </div>
+      <div class="mb-3">
+        <label for="password" class="me-2">密碼</label>
+        <input type="password" id="password" v-model="user.password" />
+      </div>
+      <input
+        type="sybmit"
+        class="btn btn-primary"
+        value="登入"
+        @click="login"
+      />
+    </form>
+  </div>
 </template>
 
 <style></style>
--- src/views/SignupView.vue
+++ src/views/SignupView.vue
@@ -5,6 +5,30 @@ export default {
   components: {
     FrontHeader,
   },
+  data() {
+    return {
+      user: {
+        email: '',
+        password: '',
+        role: 'user',
+      },
+    };
+  },
+  methods: {
+    signup() {
+      this.axios
+        .post('http://localhost:3000/signup', this.user)
+        .then(() => {
+          alert('註冊成功');
+          this.user.email = '';
+          this.user.password = '';
+          window.location.href = '#/login';
+        })
+        .catch((err) => {
+          alert(`註冊失敗：${err.response.data}`);
+        });
+    },
+  },
 };
 </script>
 
@@ -12,7 +36,25 @@ export default {
   <header>
     <FrontHeader />
   </header>
-  <h1>註冊</h1>
+  <div class="container mt-5">
+    <h1>註冊</h1>
+    <form action="#">
+      <div class="mb-3">
+        <label for="email" class="me-2">信箱</label>
+        <input type="email" id="email" v-model="user.email" />
+      </div>
+      <div class="mb-3">
+        <label for="password" class="me-2">密碼</label>
+        <input type="password" id="password" v-model="user.password" />
+      </div>
+      <input
+        type="sybmit"
+        class="btn btn-primary"
+        value="註冊"
+        @click="signup"
+      />
+    </form>
+  </div>
 </template>
 
 <style></style>
