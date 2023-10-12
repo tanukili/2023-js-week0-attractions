909e76e22ae5ec26a4c413c9d431d8813b51e437
tanukile <backpacker320@gmail.com>
2023-10-12 22:40:44 +0800

    render server嘗試08

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const jsonServer = require('json-server');
 // const auth = require('json-server-auth');
 // 建立server
 const server = jsonServer.create();
-const router = jsonServer.router('db.json');
+const router = jsonServer.router('public/db.json');
 
 // /!\ Bind the router db to the app
 server.db = router.db;
