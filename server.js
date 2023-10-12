17bd89079598731fb1b5475dba0bc4abbb451278
tanukile <backpacker320@gmail.com>
2023-10-12 22:07:00 +0800

    render servert嘗試06

--- server.js
+++ server.js
@@ -1,15 +1,13 @@
 const jsonServer = require('json-server');
 const auth = require('json-server-auth');
 
-const app = jsonServer.create();
+const server = jsonServer.create();
 const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults();
 
-app.db = router.db;
+// /!\ Bind the router db to the app
+server.db = router.db;
 
-app.use(middlewares);
-app.use(auth);
-app.use(router);
-app.listen(3000, () => {
-  console.log('JSON Server is running');
-});
+// You must apply the auth middleware before the router
+server.use(auth);
+server.use(router);
+server.listen(3000);
