efcf912f651edbeee4b30aaf16396e74a2aa2007
tanukile <backpacker320@gmail.com>
2023-10-12 19:08:11 +0800

    render server 運行module

--- vite.config.js
+++ vite.config.js
@@ -14,16 +14,3 @@ export default defineConfig({
   // 注意：上線時所用基本公共路徑
   base: '/2023-js-week0-attractions/',
 });
-
-// json Server 配置
-const jsonServer = require('json-server');
-
-const apiServer = jsonServer.create();
-const apiRouter = jsonServer.router('mock/db.json');
-const middlewares = jsonServer.defaults();
-
-apiServer.use(middlewares);
-apiServer.use(apiRouter);
-apiServer.listen(3000, () => {
-  console.log('JSON Server is running');
-});
