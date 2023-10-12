55b770dedb4c3a399454181866cfbef43986432a
tanukile <backpacker320@gmail.com>
2023-10-12 22:31:35 +0800

    render server測試07

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
+// 引入檔案
 const jsonServer = require('json-server');
-const auth = require('json-server-auth');
-
+// const auth = require('json-server-auth');
+// 建立server
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 
@@ -8,6 +9,6 @@ const router = jsonServer.router('db.json');
 server.db = router.db;
 
 // You must apply the auth middleware before the router
-server.use(auth);
+// server.use(auth);
 server.use(router);
 server.listen(3000);
