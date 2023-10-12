fe21b0ff73165e59efa1acfff0fdabdd0cc94939
tanukile <backpacker320@gmail.com>
2023-10-12 21:21:18 +0800

    render server嘗試01

---
+++ server.js
@@ -0,0 +1,15 @@
+const jsonServer = require('json-server');
+const auth = require('json-server-auth');
+
+const app = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+
+app.db = router.db;
+
+app.use(middlewares);
+app.use(auth);
+app.use(router);
+app.listen(3000, () => {
+  console.log('JSON Server is running');
+});
